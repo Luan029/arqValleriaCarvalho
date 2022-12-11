00b849a8b95b0525928cbfef02e47f1184f615a1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-12-11 10:44:09 -0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Header.css"
+
+export default props =>(
+    <React.Fragment>
+        <header>
+            <h1>Valléria Carvalho</h1>
+            <h3>Portifólio de Arquitetura</h3>
+            <div className="titulo">
+                
+            </div>
+
+        </header>
+    </React.Fragment>
+)
---
+++ src/components/main/biografia/Biografia.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Biografia.css"
+
+export default props => (
+    <React.Fragment>
+        <div className="bio">
+            <h2>Minha Biografia</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque vitae voluptates ipsam aliquid molestias tempore porro sequi sed nihil doloremque dolorum accusamus ratione voluptate, facere harum cumque illum. Molestiae.
+            </p>
+        </div>
+    </React.Fragment>
+)
